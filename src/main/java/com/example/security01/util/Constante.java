7551ca0c7f97ab6525b3c72cab06a421cfc0880a
Edwin Leon <eleonv@contraloria.gob.pe>
2023-06-24 23:38:44 -0500

    proyecto base jwt

---
+++ src/main/java/com/example/security01/util/Constante.java
@@ -0,0 +1,9 @@
+package com.example.security01.util;
+
+public class Constante {
+    public static final String RESPONSE_OK = "1";
+    public static final String RESPONSE_ERROR = "0";
+
+    public static final String ROL_ADMIN = "ROL_ADMIN";
+    public static final String ROL_USER = "ROL_USER";
+}
